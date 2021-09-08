1b0ed14c8982d3dff8f8c2052a8347d472fe67e4
Haardispro <haardmajmudar2827@gmail.com>
2021-09-08 10:42:28 +0530

    yes

--- logic.py
+++ logic.py
@@ -1,23 +1,27 @@
-#here is the logic 
-# I don't know how to do it.
-"""
 import random
 
-maths = "maths"
-science = "science"
-ss = "ss"
-english = "english"
-hindi = "hindi"
-computer = "computer"
-sports = "sports"
+#Periods
+periods = ["maths", "physics", "chemistry", "biology", "hindi", "english", "computer", "sports"]
+#print(periods[1])
 
-tt = [maths, science, ss, english, hindi, computer, sports]
-for i in range(9):
-    s = random.choice(tt)
-    print(s)
-"""
-#number of periods
-periods = int(input("Enter the number of periods"))
-for i in range(periods):
-    user_input = input("Enter a period:")
+for i in range(8):
+    #print(periods[i])
+    x = random.choice(periods)
+    print(x)
+
+
+per = 45*8
+time = 14.5 - 8.0
+print(peri/time)
+
+
+#time 
+#8:00 to 14:30
+#each period 45 min
+
+#weekdays
+days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+#on Saturday only 4 periods
+
+
 
