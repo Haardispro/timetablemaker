6e5c0f5bdf978a3bf5ac5aef02b63040e7138c01
Dev Shah <devshah1032@gmail.com>
2021-10-01 08:03:37 +0530

    Added one feature of multiple classes

--- README.md
+++ README.md
@@ -4,10 +4,12 @@
 
 > Easy to use and simple interface(cause we dont know how to make it fancy xd)
 
+> Create timetable for the whole institute
+
 > Thank you
 
 
 
 
-
+> Next feature - Classes dont clash ( May take time )
 > Status - In development
--- main.py
+++ main.py
@@ -7,9 +7,12 @@ def timetable():
     return
 
 
-institutio n = Entry(w, width=10)
+institution = Entry(w, width=10)
 institution.grid(row=1, column=0)
 periods = Entry(w, width=10)
 periods.grid(row=2, column=0)
 
 w.mainloop()
+
+#Haard do all this stuff Idk much in tkinter 
+#Just have to copy from timetablemaker.py file I made and convert into tkinter window for pleasant experience
