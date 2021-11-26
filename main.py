6a7b51fe3c54d4eacc08efaa017d4ae491637176
Haardispro <haardmajmudar2827@gmail.com>
2021-11-26 19:37:39 +0530

    added minor changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 
 w = Tk()
 w.title("Timetable maker")
+w.geometry("800x600")
 
 def timetable():
     return
@@ -11,8 +12,10 @@ institution = Entry(w, width=10)
 institution.grid(row=1, column=0)
 periods = Entry(w, width=10)
 periods.grid(row=2, column=0)
+:wq
+:wq
+:q
+:q
+exit
 
 w.mainloop()
-
-#Haard do all this stuff Idk much in tkinter 
-#Just have to copy from timetablemaker.py file I made and convert into tkinter window for pleasant experience
