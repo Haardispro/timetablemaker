66d9e838910af9f0f9b8b31a46600831341987bc
Haardispro <haardmajmudar2827@gmail.com>
2021-11-28 09:00:47 +0530

    improvements

--- timetablemaker.py
+++ timetablemaker.py
@@ -1,22 +1,26 @@
 import random 
 
-#main list
+#main list in which everything is stored
 subjects_list = []
 
+#Basic Inputs
 institution = input("What is the name of your institution?:")
 subjects = int(input("Number of Subjects:"))
 
+#Enter subjects
 for i in range(subjects):
     sub = input("Enter subject:")
     subjects_list.append(sub)
 
-
+#Number of Periods in one day
 days_periods = int(input("Number of periods in 1 day:"))
 
+#Number of working days
 days = int(input("Number of working days:"))
 
-
+#The final timetable
 print("Timetable for {}".format(institution))
+#The main loop
 for i in range(days):
     random.shuffle(subjects_list)
     if days_periods < subjects:
@@ -28,8 +32,7 @@ for i in range(days):
         print(separator.join(subjects_list))
     elif days_periods > subjects:
         #add an extra subject from the given list
-        #doing logic until conditioin in met
-
+        #doing logic until condition in met
         while len(subjects_list) != days_periods:
             x = random.randrange(0, subjects)
             y = subjects_list[x]
