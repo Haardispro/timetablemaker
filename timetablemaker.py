d3938fc192de0a370fcc3f580c89903d8b947558
Haard Majmudar <62053026+Haardispro@users.noreply.github.com>
2021-12-05 16:38:15 +0530

    Revert "idk about main.py you do Tkinter thing , I fixed timetablemaker.py as I wrote it earlier (SEE IMAGE ATTACHED)"

--- timetablemaker.py
+++ timetablemaker.py
@@ -1,39 +1,41 @@
-import os
-import random
-import time 
+import random 
 
-#os.system("figlet timetable maker")
-print("""
-     _   _                _        _     _                        _             
-    | |_(_)_ __ ___   ___| |_ __ _| |__ | | ___   _ __ ___   __ _| | _____ _ __ TM
-    | __| | '_ ` _ \ / _ \ __/ _` | '_ \| |/ _ \ | '_ ` _ \ / _` | |/ / _ \ '__|
-    | |_| | | | | | |  __/ || (_| | |_) | |  __/ | | | | | | (_| |   <  __/ |   
-     \__|_|_| |_| |_|\___|\__\__,_|_.__/|_|\___| |_| |_| |_|\__,_|_|\_\___|_|
-""")
-ran = []
-Info = input("Enter the name of your institution: ")
-periods = int(input("Enter the number of subjects in total: "))
-for i in range(periods):
-    print(f"Enter subject no.{i+1}")
-    x = input(":")
-    ran.append(x)
+#main list in which everything is stored
+subjects_list = []
 
-# Oneday = random.shuffle(ran)
-days = int(input("Enter the number of working days: "))
+#Basic Inputs
+institution = input("What is the name of your institution?:")
+subjects = int(input("Number of Subjects:"))
 
-print("Making the timetable ")
-dot = "."
-for i in range(3):
-    time.sleep(0.1)
-    print(dot)
-    time.sleep(0.2)
-    dot = dot + "."
+#Enter subjects
+for i in range(subjects):
+    sub = input("Enter subject:")
+    subjects_list.append(sub)
 
-print(f"Timetable for {Info} ->")
-for j in range(days):
-    j=j+1
-    daysList= ["Monday    ","Tuesday   ","Wednesday ","Thursday  ","Friday    ","Saturday  ","Sunday    "]
-    random.shuffle(ran)
-    print(f"{daysList[j-1]} : {ran}")
-    
-    
+#Number of Periods in one day
+days_periods = int(input("Number of periods in 1 day:"))
+
+#Number of working days
+days = int(input("Number of working days:"))
+
+#The final timetable
+print("Timetable for {}".format(institution))
+#The main loop
+for i in range(days):
+    random.shuffle(subjects_list)
+    if days_periods < subjects:
+        tt = subjects_list[0:days_periods]
+        separator = "   |   "
+        print(separator.join(tt))
+    elif days_periods == subjects:
+        separator = "   |   "
+        print(separator.join(subjects_list))
+    elif days_periods > subjects:
+        #add an extra subject from the given list
+        #doing logic until condition in met
+        while len(subjects_list) != days_periods:
+            x = random.randrange(0, subjects)
+            y = subjects_list[x]
+            subjects_list.append(y)
+        separator = "   |   "
+        print(separator.join(subjects_list))
